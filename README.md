0980fe019ef119db92526c6fc9276bb01fedf861
Kumar Abhinav <abhinavabhiav26012002@gmail.com>
2022-07-12 17:20:11 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# i am a Readme File
+
+git remote add origin https://github.com/geekabhinav007/DSA-Training.git
+git branch -M main
+git push -u origin main
---
+++ Reverse Array/resverse_array.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i = 0;
+    for (int i = 0 ; i<10 ; i++){
+        cout<<i<<endl;
+    }
+    cout<<"I am Abhinav"<<endl;
+    return 0;
+
+}
