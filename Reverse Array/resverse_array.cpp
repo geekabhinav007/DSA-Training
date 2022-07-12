94a648a85aa3cd0d965bb753985078a330afc2d2
Kumar Abhinav <abhinavabhiav26012002@gmail.com>
2022-07-13 00:44:01 +0530

    added C++ code for reverse the array

--- Reverse Array/resverse_array.cpp
+++ Reverse Array/resverse_array.cpp
@@ -1,11 +1,18 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int i = 0;
-    for (int i = 0 ; i<10 ; i++){
-        cout<<i<<endl;
+    int array[] = {1,2,3,4,5,8,10};
+    int array_size = sizeof(array)/sizeof(int);
+    int reversedArray[array_size];
+    for (int i = array_size ; i >=0 ; i--){
+        reversedArray[array_size-i] = array[i-1];
+
     }
-    cout<<"I am Abhinav"<<endl;
+    for (int i = 0; i<array_size ; i++){
+        cout<<reversedArray[i]<<" ";
+
+    }
+    cout<<endl;
     return 0;
 
 }
