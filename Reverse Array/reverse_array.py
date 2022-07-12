15df15a7e3a4cb4af044989db80515efc6faabef
Kumar Abhinav <abhinavabhiav26012002@gmail.com>
2022-07-12 23:11:03 +0530

    Added reverse_array.py
    added a file which contain program of reverse an  array or list using python

--- Reverse Array/reverse_array.py
+++ Reverse Array/reverse_array.py
@@ -1,1 +1,11 @@
-print("I am abhinav")
+# program to Reverse an array
+def reverseArray(array):
+    reversed_array = []
+    for i in range(1,len(array)+1):
+        reversed_array.append(array[-i])
+    return reversed_array
+
+if __name__ == '__main__':
+    listCumArray = [4,5,9,6,1,4,3,8,7,6,5]
+    reversed_list = reverseArray(listCumArray)
+    print(reversed_list)
