c645fbcea3e9c20ff55658f69b5f165a6c39dd70
Kumar Abhinav <abhinavabhiav26012002@gmail.com>
2022-07-15 17:49:05 +0530

    added program of circular rotation of array by 1 2nd method

--- Arrays/Circular_Rotation/Cyclically rotate an array by one.cpp
+++ Arrays/Circular_Rotation/Cyclically rotate an array by one.cpp
@@ -6,15 +6,20 @@ void circularRotation(int arr[], int size)
     // 7,2,3,4,5,9
     for (int i = size - 1; i > 0; i--)
     {
-        int temp = arr[i - 1];
-        arr[i - 1] = arr[i];
-        arr[i] = temp;
-        //swap(arr[i], arr[i - 1]);
+        swap(arr[i], arr[i - 1]);
     }
     for (int i = 0; i < size; i++)
     {
         cout << arr[i] << " ";
     }
+}
+// 2nd Mehod for rotation by 1
+void rotate(int arr[], int n)
+{
+    int x = arr[n - 1], i;
+    for (i = n - 1; i > 0; i--)
+    arr[i] = arr[i - 1]; 
+    arr[0] = x;
 }
 int main()
 {
